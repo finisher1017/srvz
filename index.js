36b3657d7c71ef88656cb74de88cf2a8f1a5ea13
Jonathan Seubert <finisher1017@gmail.com>
2019-10-24 17:50:09 -0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
+  res.send({hi: 'Hello from Node.js'});
 });
 
 const PORT = process.env.PORT || 5000;
